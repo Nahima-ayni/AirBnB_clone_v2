99add7d40ce6c9fe721a2955e522b940d3c01935
Nahima-ayni <nahimamadey1@gmail.com>
2024-03-19 20:33:09 +0300

    DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-""" User inherits from BaseModel and Base (respect the order) """
-from models.base_model import BaseModel, Base
+"""User inherits from BaseModel and Base (respect the order)"""
 from sqlalchemy import Column, String
-from sqlalchemy.orm import validates
+from .base_model import BaseModel, Base
+
 
 class User(BaseModel, Base):
-     """This class defines a user by various attributes"""
+    """
+    Represents a user in the system.
+
+    Attributes:
+        email (str): The email address of the user.
+        password (str): The password of the user.
+        first_name (str, optional): The first name of the user.
+    """
     __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-
-    @validates('email')
-    def validate_email(self, key, email):
-        assert '@' in email, "Invalid email address."
-        return email
-
-    @validates('password')
-    def validate_password(self, key, password):
-        assert len(password) >= 8, "Password must be at least 8 characters long."
-        return password
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)

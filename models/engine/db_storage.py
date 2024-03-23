d81f77381abc24b3ee2c2370b2d1ad248239ad17
Nahima-ayni <nahimamadey1@gmail.com>
2024-03-23 21:35:48 +0300

    DBStorage - States and Cities

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,96 +1,47 @@
 #!/usr/bin/python3
-"""database storage engine"""
+""" holds class User"""
+import hashlib
+import models
+from models.base_model import BaseModel, Base
 from os import getenv
-from models.base_model import Base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import text
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
-class DBStorage:
-    __engine = None
-    __session = None
+class User(BaseModel, Base):
+    """Representation of a user """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128),
+                       nullable=False)
+        _password = Column('password',
+                           String(128),
+                           nullable=False)
+        first_name = Column(String(128),
+                            nullable=True)
+        last_name = Column(String(128),
+                           nullable=True)
+        places = relationship("Place",
+                              backref="user",
+                              cascade="all, delete-orphan")
+        reviews = relationship("Review",
+                               backref="user",
+                               cascade="all, delete-orphan")
+    else:
+        email = ""
+        _password = ""
+        first_name = ""
+        last_name = ""
 
-    def __init__(self):
-        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
-        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
-        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST', default='localhost')
-        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
-        HBNB_ENV = getenv('HBNB_ENV')
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
 
-        # self.__engine = create_engine({
-        #     'engine': 'mysql+mysqldb',
-        #     'user': HBNB_MYSQL_USER,
-        #     'password': HBNB_MYSQL_PWD,
-        #     'host': HBNB_MYSQL_HOST,
-        #     'database': HBNB_MYSQL_DB,
-        #     'pool_pre_ping': True
-        # })
-        self.__engine = create_engine(
-            'mysql+mysqldb://' +
-            HBNB_MYSQL_USER +
-            ':' +
-            HBNB_MYSQL_PWD +
-            '@' +
-            HBNB_MYSQL_HOST +
-            '/' +
-            HBNB_MYSQL_DB)
+    @property
+    def password(self):
+        return self._password
 
-        if HBNB_ENV == 'test':
-            Base.metadata.drop_all(bind=self.__engine)
-
-    def all(self, cls=None):
-        classes = [User, State, City, Amenity, Place, Review]
-        objects = {}
-
-        if cls is not None:
-            if cls in classes:
-                return {obj.__class__.__name__ + '.' + obj.id:
-                        obj for obj in self.__session.query(cls).all()}
-            else:
-                return {}
-        else:
-            for c in classes:
-                for obj in self.__session.query(text(c.__name__)).all():
-                    objects[obj.__class__.__name__ + '.' + obj.id] = obj
-            return objects
-
-    def new(self, obj):
-        '''adds the obj to the current db session'''
-        if obj is not None:
-            try:
-                self.__session.add(obj)
-                self.__session.flush()
-                self.__session.refresh(obj)
-            except Exception as e:
-                self.__session.rollback()
-                raise e
-
-    def save(self):
-        '''commit all changes of the current db session'''
-        self.__session.commit()
-
-    def delete(self, obj=None):
-        ''' deletes from the current databse session the obj
-            is it's not None
-        '''
-        if obj is not None:
-            self.__session.query(type(obj)).filter(
-                type(obj).id == obj.id).delete()
-
-    def reload(self):
-        '''reloads the database'''
-        Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine,
-                                       expire_on_commit=False)
-        self.__session = scoped_session(session_factory)()
-
-    def close(self):
-        """closes the working SQLAlchemy session"""
-        self.__session.close()
+    @password.setter
+    def password(self, pwd):
+        """hashing password values"""
+        self._password = pwd

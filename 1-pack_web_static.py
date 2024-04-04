7e2d2d8e535acbcdadf59c4ad1f0adb72adab724
Nahima-ayni <nahimamadey1@gmail.com>
2024-04-04 12:29:34 +0300

    Deploy static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-""" Compress before sending """
-from fabric.api import local
-from time import strftime
-from datetime import date
+"""
+Fabric script to genereate tgz archive
+execute: fab -f 1-pack_web_static.py do_pack
+"""
+
+from datetime import datetime
+from fabric.api import *
 
 
 def do_pack():
-    """ Fabric script that generates a .tgz archive
-        from the contents of the web_static"""
-    filename = strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/".format(
-            filename))
-        return "versions/web_static_{}.tgz".format(filename)
+    """
+    making an archive on web_static folder
+    """
 
-    except Exception as e:
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
         return None

c1dd89765bc71417442b0d0befa7294d39044e3b
Nahima-ayni <nahimamadey1@gmail.com>
2024-04-19 14:40:00 +0300

    web framework

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,41 +1,39 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application:
-Your web application must be listening on 0.0.0.0, port 5000
-must use the option strict_slashes=False in your route definition
-"""
+"""Flask"""
 from flask import Flask, render_template
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def route():
-    """display Hello HBNB"""
-    return ("Hello HBNB!")
+def hello():
+    """Return a given string"""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def route2():
-    """display HBNB"""
-    return ("HBNB")
+def hbnb():
+    """Returns a given string"""
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    return "C " + text.replace("_", " ")
+def cText(text):
+    """display C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/', defaults={'text': "is cool"}, strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text):
-    return "Python " + text.replace("_", " ")
+def pythonText(text="is cool"):
+    """display Python followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    return f"{n} is a number"
+def isNumber(n):
+    """display “n is a number” only if n is an integer"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)

dec5be11102ea3a6f4fbe02c51ab6f71f97a1826
Nahima-ayni <nahimamadey1@gmail.com>
2024-04-19 10:08:50 +0300

    web framework

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -23,20 +23,19 @@ def route2():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cText(text):
-    text = text.replace('_', ' ')
-    return "c {}".format(text)
+def c(text):
+    return "C " + text.replace("_", " ")
 
 
+@app.route('/python/', defaults={'text': "is cool"}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_ext(text="is cool"):
-    text = text.replace('_', ' ')
-    return "python {}".format(text)
+def python(text):
+    return "Python " + text.replace("_", " ")
 
 
-@app.route('/number/<n>', strict_slashes=False)
-def number_n(n):
-    return ("{} is a number".format(n))
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
 
 
 if __name__ == "__main__":

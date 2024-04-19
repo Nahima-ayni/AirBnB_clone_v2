ac0b809caa658923a28d3ea89180e9526e8842f4
Nahima-ayni <nahimamadey1@gmail.com>
2024-04-19 15:56:30 +0300

    web framework

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
+"""starts a Flask web application"""
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route(‘/ states_list’, strict_slashes=False)
-def states_list():
-    states = storage.all("State").values()
-    states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states)
-
-
-@app.teardown_appcontext
-def teardown_db(exception):
+def close():
+    """ close the session """
     storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities():
+    states = storage.all("State").values()
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
